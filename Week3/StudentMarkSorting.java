c18bd4046158ae4da6ed41f69be304cdca5176b7
garlapati-sriakshitha <vtu25582@veltech.edu.in>
2026-02-05 10:29:55 +0530

    Task-01

---
+++ Week3/StudentMarkSorting.java
@@ -0,0 +1,41 @@
+package solvingjava;
+import java.util.*;
+import java.util.stream.*;
+
+class Student {
+    String name;
+    int marks;
+
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
+
+public class task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<Student> students = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            int marks = sc.nextInt();   // 👈 marks as INT
+            students.add(new Student(name, marks));
+        }
+
+        int k = sc.nextInt();
+
+        List<String> result = students.stream()
+                .sorted(Comparator
+                        .comparingInt((Student s) -> s.marks).reversed()
+                        .thenComparing(s -> s.name))
+                .limit(k)
+                .map(s -> s.name)
+                .collect(Collectors.toList());
+
+        System.out.println(String.join(" ", result));
+        sc.close();
+    }
+}
